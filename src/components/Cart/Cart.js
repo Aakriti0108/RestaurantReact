c152e42cb8dc01ee0dc610980f179fe422eef8cd
Aakriti0108 <aakritiyadav0108@gmail.com>
2024-11-05 01:33:11 +0530

    open and close cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
-const Cart = ()=>{
+const Cart = (props)=>{
 
        const cartItems = (<ul className={classes['cart-items']}>{[
         {
@@ -10,14 +10,14 @@ const Cart = ()=>{
        }</ul>);
 
      return (
-        <Modal>
+        <Modal onClick={props.onClick}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>close</button>
+                <button className={classes['button--alt']} onClick={props.onClick}>close</button>
                 <button className={classes.button}>order</button>
             </div>
         </Modal>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,9 +2,9 @@ import React from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './headerCartButton.module.css'
 
-const HeaderCartButton = () =>{
+const HeaderCartButton = (props) =>{
     return(
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
               <span className={classes.icon}><CartIcon/></span>
               <span>CART</span>
               <span className = {classes.badge}> 3 </span>
