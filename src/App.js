8857e295891a5624c88028dc85d73dc3bea39d13
Aakriti0108 <aakritiyadav0108@gmail.com>
2024-11-05 21:52:34 +0530

    cart dynamic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { React , useState } from 'react';
 import Header  from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 
 
@@ -20,13 +21,13 @@ function App() {
   
 
   return (
-    <>
+    <CartProvider>
        {cartIsShown && <Cart onClick={hideCartHandler} />}
         <Header onClick={showCartHandler}/>
         <main>
           <Meals />
         </main>
-    </>
+    </CartProvider >
 
   );
 }
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './headerCartButton.module.css'
-
+import CartContext from '../../store/cart-content';
 const HeaderCartButton = (props) =>{
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((cumNumber,item)=>{ return cumNumber + item.amount },0)
     return(
         <button className={classes.button} onClick={props.onClick}>
               <span className={classes.icon}><CartIcon/></span>
               <span>CART</span>
-              <span className = {classes.badge}> 3 </span>
+              <span className = {classes.badge}> {numberOfCartItems} </span>
         </button>
     )
 }
